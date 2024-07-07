b5ac1f3abb3741000deac049bdc64cdc7649e689
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-07-07 21:22:06 +0530

    Redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
+
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -8,8 +10,22 @@ const Demo = () => {
     summary: "",
   });
 
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+
+
+
   const handleSubmit = async (e) => {
-    alert("Submitted");
+    e.preventDefault();
+    
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
   };
 
   return (
@@ -46,6 +62,7 @@ const Demo = () => {
       </div>
     </section>
   );
+
 };
 
 export default Demo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+// main file (e.g., index.js or main.js)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './services/store.js'; // Import the store
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}> {/* Pass the store to the Provider */}
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
